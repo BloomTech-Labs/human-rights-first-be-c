980951a42fd74ded4b9d382359065654b3288dff
Jason Long <jlong5795@gmail.com>
2020-11-12 22:01:42 -0600

    Completed refactor of data storage and related queries

--- api/incidents/incidentsModel.js
+++ api/incidents/incidentsModel.js
@@ -3,28 +3,20 @@ const db = require('../../data/db-config');
 module.exports = {
   getAllIncidents,
   getIncidentById,
-  getTagById,
   createIncident,
-  getAllSources,
-  getAllTags,
-  getAllTagTypes,
-  getSourcesById,
-  createSingleSource,
   deleteDB,
-  createCategories,
 };
 
 async function getAllIncidents() {
   return await db('incidents');
 }
 
-async function getSourcesById(incident_id) {
-  return await db('sources').where('incident_id', incident_id);
-}
-
+// this set of queries will require some updates
 async function createIncident(incident) {
   const newIncident = {
-    id: incident.case_id,
+    incident_id: incident.case_id,
+    src: JSON.stringify(incident.links),
+    categories: JSON.stringify(incident.tags),
     city: incident.city,
     state: incident.state,
     title: incident.title,
@@ -39,91 +31,13 @@ async function createIncident(incident) {
     lethal_force: incident.lethal_force,
     uncategorized: incident.uncategorized,
   };
-  const incidentID = await db('incidents').insert(newIncident, 'incident_id');
-  await createSource(incident.links, incidentID[0]);
-  await createTags(incident.tags, incidentID[0]);
-  return { message: 'Success!' };
-}
-
-async function createTags(tags, incidentID) {
-  // iterate over array of tags
-  await tags.forEach(async (tag) => {
-      // for each tag, create a new typeOfForce entry
-      const tof = await createTypeOfForce(tag, incidentID);
-  });
-  return;
-}
-
-async function createSource(sources, incidentID) {
-  await sources.forEach(async (sourceURL) => {
-    const source = {
-      incident_id: incidentID,
-      src_url: sourceURL,
-    };
-    await db('sources').insert(source);
-  });
-  return;
-}
-
-async function createTypeOfForce(tof, incidentID) {
-  const incomingTOF = {type_of_force: tof};
-  
-  // check if entry already exists
-  const typeOfForce = await db('type_of_force').where('type_of_force', tof);
-  
-  if (typeOfForce.length < 1) {
-    // if yes, return it, if no, create it and return it
-    const newTof = await db('type_of_force').insert(incomingTOF, 'type_of_force_id');
-    const newLink = await db('incident_type_of_force').insert({"incident_id": incidentID, "type_of_force_id": newTof[0]})
-  
-  
-    return newLink;
-      } else {
-    return typeOfForce;
-  }
-}
-
-function getAllSources() {
-  return db('sources');
-}
-
-function getAllTags() {
-  return db('type_of_force');
-}
-
-function getAllTagTypes() {
-  return db('incident_type_of_force');
-}
-
-async function createSingleSource(source) {
-  return await db('sources').insert(source, 'src_id');
+  return db('incidents').insert(newIncident);
 }
 
 async function deleteDB() {
-  await db('incident_type_of_force').del();
-  await db('type_of_force').del();
-  await db('sources').del();
   return await db('incidents').del();
 }
 
-async function getIncidentById(id) {
-  let incident = await db('incidents').where('incident_id', id);
-  const sources = await getSourcesById(id);
-  const typeLinks = await db('incident_type_of_force').where('incident_id', id);
-  
-  return [incident, sources, typeLinks];
-}
-
-async function getTagById(id) {
-  const tag = await db('type_of_force').where('type_of_force_id', id);
-
-  return tag[0];
-}
-
-function createCategories(array) {
-  return Promise.all(array.map((link) => getTagById(link.type_of_force_id)))
-  .then(response => {
-    return response
-  })
-  .catch(error => error)
+function getIncidentById(id) {
+  return db('incidents').where('incident_id', id);
 }
--- data/migrations/20200625220949_create-profile.js
+++ data/migrations/20200625220949_create-profile.js
@@ -2,14 +2,15 @@ exports.up = function (knex) {
   return knex.schema
     .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
     .createTable('incidents', (incidents) => {
-      incidents.increments('incident_id');
-      incidents.string('id').unique().notNullable();
+      incidents.string('incident_id').unique().primary().notNullable();
+      incidents.text('src');
+      incidents.string('categories');
       incidents.string('city').notNullable();
       incidents.string('state').notNullable();
       incidents.float('lat').notNullable();
       incidents.float('long').notNullable();
       incidents.string('title').notNullable();
-      incidents.varchar('desc', [1000]);
+      incidents.varchar('desc', 10000);
       incidents.date('date');
       incidents.boolean('verbalization').defaultsTo(0);
       incidents.boolean('empty_hand_soft').defaultsTo(0);
@@ -18,26 +19,8 @@ exports.up = function (knex) {
       incidents.boolean('lethal_force').defaultsTo(0);
       incidents.boolean('uncategorized').defaultsTo(0);
     })
-    .createTable('sources', (sources) => {
-      sources.increments('src_id').notNullable().unique().primary();
-      sources.integer('incident_id');
-      sources.string('src_url');
-      sources.string('src_type');
-    })
-    .createTable('incident_type_of_force', (incident_type_of_force) => {
-      incident_type_of_force.increments('itof_id');
-      incident_type_of_force.integer('type_of_force_id').notNullable();
-      incident_type_of_force.integer('incident_id').notNullable();
-    })
-    .createTable('type_of_force', (type_of_force) => {
-      type_of_force.increments('type_of_force_id');
-      type_of_force.string('type_of_force');
-    });
 };
 exports.down = function (knex) {
   return knex.schema
-    .dropTableIfExists('incidents')
-    .dropTableIfExists('sources')
-    .dropTableIfExists('incident_type_of_force')
-    .dropTableIfExists('type_of_force');
+    .dropTableIfExists('incidents');
 };
